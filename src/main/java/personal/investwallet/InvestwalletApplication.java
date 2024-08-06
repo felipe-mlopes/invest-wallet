eedddc1d3e37aa36f715645bbd6a5f584bd88e87
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-08-06 07:54:32 -0300

    first commit

---
+++ src/main/java/personal/investwallet/InvestwalletApplication.java
@@ -0,0 +1,13 @@
+package personal.investwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestwalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personal/investwallet/InvestwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.investwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
