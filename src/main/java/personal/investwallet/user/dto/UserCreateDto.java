36c2ae6800e28b3c5531f66933dcd35ee0a546f2
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-08-06 21:50:15 +0000

    feat: create user create dto

---
+++ src/main/java/personal/investwallet/user/dto/UserCreateDto.java
@@ -0,0 +1,17 @@
+package personal.investwallet.user.dto;
+
+import jakarta.validation.constraints.*;
+
+public record UserCreateDto(
+    @NotBlank(message = "O nome não pode ser vazio")
+    @Size(min = 2, max = 30, message = "O nome deve ter entre 2 e 30 caracteres")
+    String name,
+
+    @NotBlank(message = "O e-mail não pode ser vazio")
+    @Email(message = "Formato inválido de email")
+    String email,
+
+    @NotBlank(message = "A senha não pode ser vazia")
+    @Size(min = 6, max = 12, message = "A senha deve ter entre 6 e 12 caracteres")
+    String password
+) {}
