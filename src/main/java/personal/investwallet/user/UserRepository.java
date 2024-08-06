84fcad646cc2d604f0d81fa8818555555c304d69
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-08-06 21:49:46 +0000

    feat: create user repository

---
+++ src/main/java/personal/investwallet/user/UserRepository.java
@@ -0,0 +1,13 @@
+package personal.investwallet.user;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    public Optional<UserEntity> findByEmail(String email);
+}
