e6a1cddc40edc7803a49e713fddd711a23b8cd03
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-08-24 14:26:04 -0300

    refactor: add Filter implements, include LOGGER_TENANT_ID, PRIVATE_TENANT_HEADER and DEFAULT_TENANT variables, update currentTenant and create doFilter method

---
+++ src/main/java/personal/investwallet/config/TenantContext.java
@@ -0,0 +1,50 @@
+package personal.investwallet.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@Component
+public class TenantContext implements Filter {
+
+    private static final String LOGGER_TENANT_ID = "tenant_id";
+    public static final String PRIVATE_TENANT_HEADER = "X-PrivateTenant";
+    private static final String DEFAULT_TENANT = "public";
+
+    private static final ThreadLocal<String> currentTenant = new InheritableThreadLocal<>();
+
+    public static String getCurrentTenant() {
+        String tenant = currentTenant.get();
+        return Objects.requireNonNullElse(tenant, DEFAULT_TENANT);
+    }
+
+    public static void setCurrentTenant(String tenant) {
+        MDC.put(LOGGER_TENANT_ID, tenant);
+        currentTenant.set(tenant);
+    }
+
+    public static void clear() {
+        MDC.clear();
+        currentTenant.remove();
+    }
+
+    @SuppressWarnings("unused")
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String privateTenant = req.getHeader(PRIVATE_TENANT_HEADER);
+
+        if (privateTenant != null) {
+            TenantContext.setCurrentTenant(privateTenant);
+        }
+        chain.doFilter(request, response);
+    }
+}
