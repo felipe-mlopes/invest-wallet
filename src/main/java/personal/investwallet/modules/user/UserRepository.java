1917042b723aa879f66401c7c5a96ce86934537e
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-09-28 21:42:03 -0300

    refactor: change JpaRepository to MongoRepository, switch UUID to string in MongoRepository params and remove public of the findByEmail method

--- src/main/java/personal/investwallet/modules/user/UserRepository.java
+++ src/main/java/personal/investwallet/modules/user/UserRepository.java
@@ -1,13 +1,12 @@
 package personal.investwallet.modules.user;
 
 import java.util.Optional;
-import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+public interface UserRepository extends MongoRepository<UserEntity, String> {
 
-    public Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByEmail(String email);
 }
