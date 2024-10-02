2b7687673ffa1d53e4dd0b23259cf32ee3c08330
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-10-02 08:00:20 -0300

    feat: create scraper entity

---
+++ src/main/java/personal/investwallet/modules/webscraper/ScraperEntity.java
@@ -0,0 +1,13 @@
+package personal.investwallet.modules.webscraper;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ScraperEntity {
+
+    private String yieldValue;
+}
