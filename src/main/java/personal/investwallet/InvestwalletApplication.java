74a798700d814310dc7d90ffbd88f0f84805d375
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-10-02 07:57:57 -0300

    feat: add EnableFeignClients annotation

--- src/main/java/personal/investwallet/InvestwalletApplication.java
+++ src/main/java/personal/investwallet/InvestwalletApplication.java
@@ -3,11 +3,13 @@ package personal.investwallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableFeignClients
 public class InvestwalletApplication {
 
 	public static void main(String[] args) {
