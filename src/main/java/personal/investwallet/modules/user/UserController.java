c14850b577cbb976799bbdab10c9f003c02683f7
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-10-07 23:43:47 -0300

    refactor: update CreateResponseDTO to CreateUserResponseDto and message to result in create route method, add tokenService's addTokenToCookies and update TokenResponseDto to TokenResponseDto in login route method

--- src/main/java/personal/investwallet/modules/user/UserController.java
+++ src/main/java/personal/investwallet/modules/user/UserController.java
@@ -1,5 +1,6 @@
 package personal.investwallet.modules.user;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,10 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
-import personal.investwallet.modules.user.dto.CreateResponseDTO;
-import personal.investwallet.modules.user.dto.TokenResponseDTO;
+import personal.investwallet.modules.user.dto.CreateUserResponseDto;
+import personal.investwallet.modules.user.dto.TokenResponseDto;
 import personal.investwallet.modules.user.dto.UserCreateRequestDto;
 import personal.investwallet.modules.user.dto.UserLoginRequestDto;
+import personal.investwallet.security.TokenService;
 
 @RestController
 @RequestMapping("/user")
@@ -20,20 +22,24 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping("/register")
-    public ResponseEntity<CreateResponseDTO> create(@Valid @RequestBody UserCreateRequestDto payload) {
+    public ResponseEntity<CreateUserResponseDto> create(@Valid @RequestBody UserCreateRequestDto payload) {
 
-        var message = userService.createUser(payload);
+        String result = userService.createUser(payload);
 
-        return ResponseEntity.created(null).body(new CreateResponseDTO(message));
+        return ResponseEntity.created(null).body(new CreateUserResponseDto(result));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponseDTO> login(@Valid @RequestBody UserLoginRequestDto payload) {
+    public ResponseEntity<TokenResponseDto> login(@Valid @RequestBody UserLoginRequestDto payload, HttpServletResponse response) {
 
-        var token = userService.authUser(payload);
+        String token = userService.authUser(payload, response);
+        tokenService.addTokenToCookies(token, response);
 
-        return ResponseEntity.ok(new TokenResponseDTO(token));
+        return ResponseEntity.ok(new TokenResponseDto(token));
     }
 
 }
