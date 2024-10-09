b1eb270c4c04034d67660794ff7bf2b2f181d3b6
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-10-08 22:42:06 -0300

    feat: add assetName and assetType in params

--- src/main/java/personal/investwallet/modules/wallet/dto/SalesInfoRequestDto.java
+++ src/main/java/personal/investwallet/modules/wallet/dto/SalesInfoRequestDto.java
@@ -3,5 +3,5 @@ package personal.investwallet.modules.wallet.dto;
 import java.math.BigDecimal;
 import java.time.Instant;
 
-public record SalesInfoRequestDto(int saleAmount, BigDecimal salePrice, Instant saleDate) {
+public record SalesInfoRequestDto(String assetName, String assetType, int saleAmount, BigDecimal salePrice, Instant saleDate) {
 }
