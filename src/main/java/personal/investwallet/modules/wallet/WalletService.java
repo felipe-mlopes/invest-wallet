444202de1f56c33ce238c88118c07e40db765a62
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-10-08 22:49:11 -0300

    refactor: change RuntimeException to exception handlers, implement rest of the logic in addAssetToWallet service method, change registerPurchaseOfAnExistingAsset to addPurchaseToAsset service method name and update walletRepository method, add addSaleToAsset service method and remove unused convertToAssetEntity private method

--- src/main/java/personal/investwallet/modules/wallet/WalletService.java
+++ src/main/java/personal/investwallet/modules/wallet/WalletService.java
@@ -2,6 +2,10 @@ package personal.investwallet.modules.wallet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import personal.investwallet.exceptions.BadRequestException;
+import personal.investwallet.exceptions.ConflictException;
+import personal.investwallet.exceptions.ForbiddenException;
+import personal.investwallet.exceptions.ResourceNotFoundException;
 import personal.investwallet.modules.user.UserRepository;
 import personal.investwallet.modules.wallet.dto.AssetCreateRequestDto;
 import personal.investwallet.modules.wallet.dto.PurchasesInfoRequestDto;
@@ -10,7 +14,6 @@ import personal.investwallet.modules.webscraper.ScraperService;
 import personal.investwallet.security.TokenService;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static personal.investwallet.modules.wallet.WalletEntity.*;
 import static personal.investwallet.modules.wallet.WalletEntity.Asset.*;
@@ -53,39 +56,52 @@ public class WalletService {
         boolean isAssetExists = verifyAssetNameExists(payload.assetType(), payload.assetName());
 
         if (!isAssetExists)
-            throw new RuntimeException("O ativo informado não existe.");
+            throw new ResourceNotFoundException("O ativo informado não existe.");
 
         Optional<WalletEntity> wallet = walletRepository.findByUserId(userId);
 
+        Asset newAsset = new Asset(
+                payload.assetName(),
+                payload.quotaAmount(),
+                new HashSet<>(),
+                new HashSet<>()
+        );
+
         if (wallet.isPresent()) {
 
-            // Implementar a lógica de adicionar um novo asset na carteira existente
+            if (wallet.get().getAsset().containsKey(payload.assetName()))
+                throw new ConflictException("O ativo informado já existe na carteira");
+
+            walletRepository.addNewAssetByUserId(userId, newAsset.getAssetName(), newAsset);
 
         } else {
 
-            // Criar uma carteira para userId adicionando o asset informado
+            WalletEntity newWallet = new WalletEntity();
+            newWallet.setUserId(userId);
+            newWallet.getAsset().put(newAsset.getAssetName(), newAsset);
 
+            walletRepository.save(newWallet);
         }
 
         return "Ativo adicionado à carteira com sucesso!";
     }
 
-    public String registerPurchaseOfAnExistingAsset(String token, PurchasesInfoRequestDto payload) {
+    public String addPurchaseToAsset(String token, PurchasesInfoRequestDto payload) {
 
         String userId = getUserId(token);
 
         boolean isAssetExists = verifyAssetNameExists(payload.assetType(), payload.assetName());
 
         if (!isAssetExists)
-            throw new RuntimeException("O ativo informado não existe.");
+            throw new ResourceNotFoundException("O ativo informado não existe.");
 
         WalletEntity wallet = walletRepository.findByUserId(userId)
-                .orElseThrow(() -> new RuntimeException("Carteira não encontrada para o usuário informado."));
+                .orElseThrow(() -> new ResourceNotFoundException("Carteira não encontrada para o usuário informado."));
 
         Asset asset = wallet.getAsset().get(payload.assetName());
 
         if (asset == null)
-            throw new RuntimeException("O ativo informado não existe na carteira");
+            throw new ForbiddenException("O ativo informado não existe na carteira");
 
         PurchasesInfo newPurchase = new PurchasesInfo(
                 payload.purchaseAmount(),
@@ -93,9 +109,42 @@ public class WalletService {
                 payload.purchaseDate()
         );
 
-        walletRepository.updatePurchasesInfo(userId, asset.getAssetName(), newPurchase, payload.purchaseAmount());
+        walletRepository.addPurchaseToAssetByUserIdAndAssetName(userId, asset.getAssetName(), newPurchase, payload.purchaseAmount());
 
-        return "A compra do seu ativo " + payload.assetName() + " foi cadastrada com sucesso" ;
+        return "A compra do seu ativo " + asset.getAssetName() + " foi cadastrada com sucesso" ;
+    }
+
+    public String addSaleToAsset(String token, SalesInfoRequestDto payload) {
+
+        String userId = getUserId(token);
+
+        boolean isAssetExists = verifyAssetNameExists(payload.assetType(), payload.assetName());
+
+        if (!isAssetExists)
+            throw new ResourceNotFoundException("O ativo informado não existe.");
+
+        WalletEntity wallet = walletRepository.findByUserId(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Carteira não encontrada para o usuário informado."));
+
+        Asset asset = wallet.getAsset().get(payload.assetName());
+
+        if (asset == null)
+            throw new ForbiddenException("O ativo informado não existe na carteira.");
+
+        int saleAmount = payload.saleAmount() * -1;
+
+        if (asset.getQuotaAmount() + saleAmount < 0)
+            throw new BadRequestException("A quantidade de cota do ativo não pode ser negativa.");
+
+        SalesInfo newSale = new SalesInfo(
+                saleAmount,
+                payload.salePrice(),
+                payload.saleDate()
+        );
+
+        walletRepository.addSaleToAssetByUserIdAndAssetName(userId, asset.getAssetName(), newSale, payload.saleAmount());
+
+        return "A venda do seu ativo " + asset.getAssetName() + " foi cadastrada com sucesso." ;
     }
 
     private String getUserId(String token) {
@@ -110,20 +159,5 @@ public class WalletService {
         return scraperService.verifyIfWebsiteIsValid(assetType, assetName);
     }
 
-    private Asset convertToAssetEntity(AssetCreateRequestDto asset) {
-        Set<PurchasesInfo> purchasesInfo = (asset.purchasesInfo() == null) ? new HashSet<>() : asset.purchasesInfo().stream()
-                .map(WalletService::purchases).collect(Collectors.toSet());
-
-        Set<SalesInfo> salesInfo = (asset.salesInfo() == null) ? new HashSet<>() : asset.salesInfo().stream()
-                .map(WalletService::sales).collect(Collectors.toSet());
-
-        return new WalletEntity.Asset(
-                asset.assetName(),
-                asset.quotaAmount(),
-                purchasesInfo,
-                salesInfo
-        );
-
-    }
 }
 
