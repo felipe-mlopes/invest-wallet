cc06a461fb1dbc4942f9dd3739e4c2041a38e5b1
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-10-13 20:48:29 -0300

    feat: create user validate request dto

---
+++ src/main/java/personal/investwallet/modules/user/dto/UserValidateRequestDto.java
@@ -0,0 +1,14 @@
+package personal.investwallet.modules.user.dto;
+
+import jakarta.validation.constraints.*;
+
+public record UserValidateRequestDto(
+        @NotBlank(message = "O e-mail não pode ser vazio")
+        @Email(message = "Formato inválido de email")
+        String email,
+
+        @NotBlank(message = "O código de verificação não pode ser vazio")
+        @Pattern(regexp = "\\d{4}", message = "O código de verificação deve conter 4 dígitos")
+        String code
+) {
+}
