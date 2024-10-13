b3a56c7dfcc5749aa150a7c97fa92f2416735a28
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-10-13 20:34:42 -0300

    feat: add mongoTemplate method

--- src/main/java/personal/investwallet/config/database/MongoConfig.java
+++ src/main/java/personal/investwallet/config/database/MongoConfig.java
@@ -3,6 +3,9 @@ package personal.investwallet.config.database;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
@@ -19,5 +22,10 @@ public class MongoConfig {
     @Bean
     public ValidatingMongoEventListener validatingMongoEventListener(LocalValidatorFactoryBean localValidatorFactoryBean) {
         return new ValidatingMongoEventListener(localValidatorFactoryBean);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDbFactory, MongoMappingContext context) {
+        return new MongoTemplate(mongoDbFactory);
     }
 }
