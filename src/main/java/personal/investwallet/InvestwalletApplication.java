a647fcc0a3f554c390e26078afb2d4fbf6870132
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-10-13 20:34:19 -0300

    feat: add EnableAsync and EnableCaching annotations

--- src/main/java/personal/investwallet/InvestwalletApplication.java
+++ src/main/java/personal/investwallet/InvestwalletApplication.java
@@ -3,8 +3,10 @@ package personal.investwallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableFeignClients
 @EnableScheduling
+@EnableAsync
+@EnableCaching
 public class InvestwalletApplication {
 
 	public static void main(String[] args) {
