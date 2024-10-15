106a55f4b46a9eca5090679616de44de4d97bf10
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-10-15 12:19:57 -0300

    feat: add updatePurchaseInAssetByPurchaseId and updateSaleInAssetBySaleId methods

--- src/main/java/personal/investwallet/modules/wallet/WalletRepository.java
+++ src/main/java/personal/investwallet/modules/wallet/WalletRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -22,9 +23,17 @@ public interface WalletRepository extends MongoRepository<WalletEntity, String> 
 
     @Query("{ 'userId': ?0, 'asset.?1.assetName': ?1 }")
     @Update("{ '$push': { 'asset.?1.salesInfo': ?2 }, '$inc': { 'asset.?1.quotaAmount': ?3 } }")
-    void addSaleToAssetByUserIdAndAssetName(String userId, String assetName, WalletEntity.Asset.SalesInfo salesInfo, int quotaIncrement);
+    void addSaleToAssetByUserIdAndAssetName(String userId, String assetName, WalletEntity.Asset.SalesInfo salesInfo, int quotaDecrease);
 
     @Query("{ 'userId': ?0, 'asset.?1.assetName': ?1 }")
     @Update("{ '$inc': { 'asset.?1.quotaAmount': ?2 } }")
     void restoreAmountOfQuotasInAsset(String userId, String assetName, String purchaseId, int previousPurchaseAmount);
+
+    @Query("{ 'userId': ?0, 'asset.?1.assetName': ?1 }")
+    @Update("{ '$set': { 'asset.?1.purchasesInfo': ?2 }, '$inc': { 'asset.?1.quotaAmount': ?3 } }")
+    void updatePurchaseInAssetByPurchaseId(String userId, String assetName, List<WalletEntity.Asset.PurchasesInfo> purchasesInfo, int quotaDecrease);
+
+    @Query("{ 'userId': ?0, 'asset.?1.assetName': ?1 }")
+    @Update("{ '$set': { 'asset.?1.salesInfo': ?2 }, '$inc': { 'asset.?1.quotaAmount': ?3 } }")
+    void updateSaleInAssetBySaleId(String userId, String assetName, List<WalletEntity.Asset.SalesInfo> salesInfo, int quotaIncrement);
 }
