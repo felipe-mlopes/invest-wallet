cea327392a9f02dbf79e323cda0c1454bfd2056c
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-10-15 19:31:08 -0300

    feat: add compound index annotation

--- src/main/java/personal/investwallet/modules/wallet/WalletEntity.java
+++ src/main/java/personal/investwallet/modules/wallet/WalletEntity.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CompoundIndex(def = "{ userId: 1, assetName: 1 }", name = "idx0")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
