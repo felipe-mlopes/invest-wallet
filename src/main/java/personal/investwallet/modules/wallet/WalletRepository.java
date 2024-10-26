43b3c5c434707ed3670044c342d6e88edc9f2d1d
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-10-26 20:40:21 -0300

    fix: change asset to assets

--- src/main/java/personal/investwallet/modules/wallet/WalletRepository.java
+++ src/main/java/personal/investwallet/modules/wallet/WalletRepository.java
@@ -16,26 +16,26 @@ public interface WalletRepository extends MongoRepository<WalletEntity, String> 
     Optional<WalletEntity> findByUserId(String userId);
     
     @Query("{ 'userId': ?0 }")
-    @Update("{ '$set': { 'asset.?1': ?2 } }")
+    @Update("{ '$set': { 'assets.?1': ?2 } }")
     void addNewAssetByUserId(String userId, String assetName, WalletEntity.Asset newAsset);
 
-    @Query("{ 'userId': ?0, 'asset.?1.assetName': ?1 }")
-    @Update("{ '$push': { 'asset.?1.purchasesInfo': ?2 }, '$inc': { 'asset.?1.quotaAmount': ?3 } }")
+    @Query("{ 'userId': ?0, 'assets.?1.assetName': ?1 }")
+    @Update("{ '$push': { 'assets.?1.purchasesInfo': ?2 }, '$inc': { 'assets.?1.quotaAmount': ?3 } }")
     void addPurchaseToAssetByUserIdAndAssetName(String userId, String assetName, WalletEntity.Asset.PurchasesInfo purchasesInfo, int quotaIncrement);
 
-    @Query("{ 'userId': ?0, 'asset.?1.assetName': ?1 }")
-    @Update("{ '$push': { 'asset.?1.salesInfo': ?2 }, '$inc': { 'asset.?1.quotaAmount': ?3 } }")
+    @Query("{ 'userId': ?0, 'assets.?1.assetName': ?1 }")
+    @Update("{ '$push': { 'assets.?1.salesInfo': ?2 }, '$inc': { 'assets.?1.quotaAmount': ?3 } }")
     void addSaleToAssetByUserIdAndAssetName(String userId, String assetName, WalletEntity.Asset.SalesInfo salesInfo, int quotaDecrease);
 
-    @Query("{ 'userId': ?0, 'asset.?1.assetName': ?1 }")
-    @Update("{ '$inc': { 'asset.?1.quotaAmount': ?2 } }")
+    @Query("{ 'userId': ?0, 'assets.?1.assetName': ?1 }")
+    @Update("{ '$inc': { 'assets.?1.quotaAmount': ?2 } }")
     void restoreAmountOfQuotasInAsset(String userId, String assetName, int previousPurchaseAmount);
 
-    @Query("{ 'userId': ?0, 'asset.?1.assetName': ?1 }")
-    @Update("{ '$set': { 'asset.?1.purchasesInfo': ?2 }, '$inc': { 'asset.?1.quotaAmount': ?3 } }")
+    @Query("{ 'userId': ?0, 'assets.?1.assetName': ?1 }")
+    @Update("{ '$set': { 'assets.?1.purchasesInfo': ?2 }, '$inc': { 'assets.?1.quotaAmount': ?3 } }")
     void updatePurchaseInAssetByPurchaseId(String userId, String assetName, List<WalletEntity.Asset.PurchasesInfo> purchasesInfo, int quotaDecrease);
 
-    @Query("{ 'userId': ?0, 'asset.?1.assetName': ?1 }")
-    @Update("{ '$set': { 'asset.?1.salesInfo': ?2 }, '$inc': { 'asset.?1.quotaAmount': ?3 } }")
+    @Query("{ 'userId': ?0, 'assets.?1.assetName': ?1 }")
+    @Update("{ '$set': { 'assets.?1.salesInfo': ?2 }, '$inc': { 'assets.?1.quotaAmount': ?3 } }")
     void updateSaleInAssetBySaleId(String userId, String assetName, List<WalletEntity.Asset.SalesInfo> salesInfo, int quotaIncrement);
 }
