26709934f27aa8210e8627bcd76ef0afbc183487
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-11-06 20:59:04 -0300

    test: create invalid string format exception

---
+++ src/main/java/personal/investwallet/exceptions/InvalidStringFormatException.java
@@ -0,0 +1,10 @@
+package personal.investwallet.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidStringFormatException extends RuntimeException {
+
+    public InvalidStringFormatException(String message) { super(message); }
+}
