eb499569d4d3c5dd910f514e934d851acd63d77f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-11-08 22:22:27 -0300

    refactor: add @NotBlank and @Size validations in assetName, change int to Integer and add @Positive validation in saleAmount, include @Positive validation in salePrice and add @PastOrPresent in saleDate

--- src/main/java/personal/investwallet/modules/wallet/dto/AddSaleRequestDto.java
+++ src/main/java/personal/investwallet/modules/wallet/dto/AddSaleRequestDto.java
@@ -1,7 +1,25 @@
 package personal.investwallet.modules.wallet.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 
-public record AddSaleRequestDto(String assetName, int saleAmount, BigDecimal salePrice, Instant saleDate) {
+public record AddSaleRequestDto(
+        @NotBlank(message = "O nome do ativo não pode ser vazio")
+        @Size(min = 5, max = 6, message = "O nome do ativo deve conter entre 5 e 6 caracteres")
+        String assetName,
+
+        @Positive(message = "O número de cotas vendidas deve ser maior que zero")
+        Integer saleAmount,
+
+        @Positive(message = "O preço da venda deve ser maior que zero")
+        BigDecimal salePrice,
+
+        @PastOrPresent(message = "A data da venda não pode ser no futuro")
+        Instant saleDate
+) {
 }
