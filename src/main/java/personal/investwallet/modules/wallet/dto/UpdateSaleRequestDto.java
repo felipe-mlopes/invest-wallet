3efd135c8df503e28e3251e3c281d0422161b995
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-11-08 22:23:07 -0300

    feat: add @Positive validation in saleAmount and in salePrice and add @PastOrPresent in saleDate

--- src/main/java/personal/investwallet/modules/wallet/dto/UpdateSaleRequestDto.java
+++ src/main/java/personal/investwallet/modules/wallet/dto/UpdateSaleRequestDto.java
@@ -1,11 +1,19 @@
 package personal.investwallet.modules.wallet.dto;
 
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 
 public record UpdateSaleRequestDto(
+        @Positive(message = "O número de cotas vendidas deve ser maior que zero")
         Integer saleAmount,
+
+        @Positive(message = "O preço da venda deve ser maior que zero")
         BigDecimal salePrice,
+
+        @PastOrPresent(message = "A data da venda não pode ser no futuro")
         Instant saleDate
 ) {
 }
