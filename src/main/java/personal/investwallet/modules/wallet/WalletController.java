a93a8386d5969c2ffbc508f10722be04086da233
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-11-09 22:24:48 -0300

    refactor: change CreateWalletResponseDto and UpdateWalletResponseDto to WallerSuccessResponseDto, and switch ResponseEntity created to ok in addManyPurchasesByCSV and addManySalesByCSV methods

--- src/main/java/personal/investwallet/modules/wallet/WalletController.java
+++ src/main/java/personal/investwallet/modules/wallet/WalletController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import personal.investwallet.modules.wallet.dto.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/wallet")
 public class WalletController {
@@ -17,62 +15,40 @@ public class WalletController {
     private WalletService walletService;
 
     @PostMapping()
-    public ResponseEntity<CreateWalletResponseDto> create(
+    public ResponseEntity<WallerSuccessResponseDto> create(
             @CookieValue(value = "access_token") String token,
             @Valid @RequestBody CreateAssetRequestDto payload
     ) {
 
         String result = walletService.addAssetToWallet(token, payload);
 
-        return ResponseEntity.created(null).body(new CreateWalletResponseDto(result));
+        return ResponseEntity.created(null).body(new WallerSuccessResponseDto(result));
     }
 
     @PostMapping("/purchase")
-    public ResponseEntity<UpdateWalletResponseDto> addPurchase(
+    public ResponseEntity<WallerSuccessResponseDto> addPurchase(
             @CookieValue(value = "access_token") String token,
             @Valid @RequestBody AddPurchaseRequestDto payload
             ) {
 
         String result = walletService.addPurchaseToAsset(token, payload);
 
-        return ResponseEntity.ok(new UpdateWalletResponseDto(result));
+        return ResponseEntity.ok(new WallerSuccessResponseDto(result));
     }
 
     @PostMapping("/purchases")
-    public ResponseEntity<String> addManyPurchasesByCSV(
+    public ResponseEntity<WallerSuccessResponseDto> addManyPurchasesByCSV(
             @CookieValue(value = "access_token") String token,
             MultipartFile file
     ) {
 
         String result = walletService.addAllPurchasesToAssetByFile(token, file);
 
-        return ResponseEntity.created(null).body(result);
-    }
-
-    @PostMapping("/sale")
-    public ResponseEntity<UpdateWalletResponseDto> addSale(
-            @CookieValue(value = "access_token") String token,
-            @Valid @RequestBody AddSaleRequestDto payload
-    ) {
-
-        String result = walletService.addSaleToAsset(token, payload);
-
-        return ResponseEntity.ok(new UpdateWalletResponseDto(result));
-    }
-
-    @PostMapping("/sales")
-    public ResponseEntity<String> addManySalesByCSV(
-            @CookieValue(value = "access_token") String token,
-            MultipartFile file
-    ) {
-
-        String result = walletService.addAllSalesToAssetByFile(token, file);
-
-        return ResponseEntity.created(null).body(result);
+        return ResponseEntity.ok(new WallerSuccessResponseDto(result));
     }
 
     @PatchMapping("/{assetType}/{assetName}/purchases/{purchaseId}")
-    public ResponseEntity<UpdateWalletResponseDto> updatePurchase(
+    public ResponseEntity<WallerSuccessResponseDto> updatePurchase(
             @CookieValue(value = "access_token") String token,
             @PathVariable String assetName,
             @PathVariable String purchaseId,
@@ -86,11 +62,45 @@ public class WalletController {
                 payload
         );
 
-        return ResponseEntity.ok(new UpdateWalletResponseDto(result));
+        return ResponseEntity.ok(new WallerSuccessResponseDto(result));
+    }
+
+    @DeleteMapping("/{assetType}/{assetName}/purchases/{purchaseId}")
+    public ResponseEntity<WallerSuccessResponseDto> removePurchase(
+            @CookieValue(value = "access_token") String token,
+            @PathVariable String assetName,
+            @PathVariable String purchaseId
+    ) {
+
+        String result = walletService.removePurchaseToAssetByPurchaseId(token, assetName, purchaseId);
+
+        return ResponseEntity.ok(new WallerSuccessResponseDto(result));
+    }
+
+    @PostMapping("/sale")
+    public ResponseEntity<WallerSuccessResponseDto> addSale(
+            @CookieValue(value = "access_token") String token,
+            @Valid @RequestBody AddSaleRequestDto payload
+    ) {
+
+        String result = walletService.addSaleToAsset(token, payload);
+
+        return ResponseEntity.ok(new WallerSuccessResponseDto(result));
+    }
+
+    @PostMapping("/sales")
+    public ResponseEntity<WallerSuccessResponseDto> addManySalesByCSV(
+            @CookieValue(value = "access_token") String token,
+            MultipartFile file
+    ) {
+
+        String result = walletService.addAllSalesToAssetByFile(token, file);
+
+        return ResponseEntity.ok(new WallerSuccessResponseDto(result));
     }
 
     @PatchMapping("/{assetType}/{assetName}/sales/{saleId}")
-    public ResponseEntity<UpdateWalletResponseDto> updateSale(
+    public ResponseEntity<WallerSuccessResponseDto> updateSale(
             @CookieValue(value = "access_token") String token,
             @PathVariable String assetName,
             @PathVariable String saleId,
@@ -104,23 +114,11 @@ public class WalletController {
                 payload
         );
 
-        return ResponseEntity.ok(new UpdateWalletResponseDto(result));
-    }
-
-    @DeleteMapping("/{assetType}/{assetName}/purchases/{purchaseId}")
-    public ResponseEntity<UpdateWalletResponseDto> removePurchase(
-            @CookieValue(value = "access_token") String token,
-            @PathVariable String assetName,
-            @PathVariable String purchaseId
-    ) {
-
-        String result = walletService.removePurchaseToAssetByPurchaseId(token, assetName, purchaseId);
-
-        return ResponseEntity.ok(new UpdateWalletResponseDto(result));
+        return ResponseEntity.ok(new WallerSuccessResponseDto(result));
     }
 
     @DeleteMapping("/{assetType}/{assetName}/sales/{saleId}")
-    public ResponseEntity<UpdateWalletResponseDto> removeSale(
+    public ResponseEntity<WallerSuccessResponseDto> removeSale(
             @CookieValue(value = "access_token") String token,
             @PathVariable String assetName,
             @PathVariable String saleId
@@ -128,6 +126,6 @@ public class WalletController {
 
         String result = walletService.removeSaleToAssetBySaleId(token, assetName, saleId);
 
-        return ResponseEntity.ok(new UpdateWalletResponseDto(result));
+        return ResponseEntity.ok(new WallerSuccessResponseDto(result));
     }
 }
