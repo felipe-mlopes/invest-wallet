cffcc4ebac739a3104343ce344d94f635d9dea8f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-11-15 21:38:09 -0300

    refactor: remove @CompoundIndexes, change unique to useGeneratedName indexed in userId, delete yieldByAssetName and yieldByPaymentAt and update YieldInfo

--- src/main/java/personal/investwallet/modules/yield/YieldEntity.java
+++ src/main/java/personal/investwallet/modules/yield/YieldEntity.java
@@ -3,8 +3,6 @@ package personal.investwallet.modules.yield;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.index.CompoundIndex;
-import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
@@ -13,50 +11,44 @@ import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@CompoundIndexes({
-        @CompoundIndex(name = "user_asset_idx", def = "{'userId': 1, 'yieldByAssetName.assetName': 1}"),
-        @CompoundIndex(name = "yield_at_idx", def = "{'userId': 1, 'yieldByAssetName.assetName': 1, 'yield.yieldAt': 1}"),
-})
 @Document(collection = "yields")
 public class YieldEntity {
 
     @MongoId(FieldType.OBJECT_ID)
     private String id;
 
-    @Indexed(unique = true)
+    @Indexed(useGeneratedName = true)
     @Field("user_id")
     private String userId;
 
-    @Field("yield_by_asset_name")
-    private Map<String, Map<String, YieldInfo>> yieldByAssetName = new HashMap<>();
+    @Field("asset_name")
+    private String assetName;
 
-    @Field("yield_by_payment_at")
-    private Map<String, Map<String, YieldInfo>> yieldByPaymentAt = new HashMap<>();
+    @Indexed(useGeneratedName = true)
+    @Field("yield_at")
+    private String yieldAt;
 
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class YieldInfo {
+    @Indexed(unique = true)
+    @Field("user_asset_yield_at")
+    private String userAssetYieldAt;
 
-        @Field("base_date")
-        private Instant baseDate;
+    @Field("base_date")
+    private Instant baseDate;
 
-        @Field("payment_date")
-        private Instant paymentDate;
+    @Field("payment_date")
+    private Instant paymentDate;
 
-        @Field("base_price")
-        private BigDecimal basePrice;
+    @Field("base_price")
+    private BigDecimal basePrice;
 
-        @Field("income_value")
-        private BigDecimal incomeValue;
+    @Field("income_value")
+    private BigDecimal incomeValue;
 
-        @Field("yield_value")
-        private BigDecimal yieldValue;
-    }
+    @Field("yield_value")
+    private BigDecimal yieldValue;
+
 }
