394440b9a343f721cf183a8787f82183f472b76d
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-11-15 21:39:39 -0300

    refactor: remove findByUserId method and include existsByUserAssetYieldAt and findByUserIdAndYieldAt method

--- src/main/java/personal/investwallet/modules/yield/YieldRepository.java
+++ src/main/java/personal/investwallet/modules/yield/YieldRepository.java
@@ -1,13 +1,16 @@
 package personal.investwallet.modules.yield;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface YieldRepository extends MongoRepository<YieldEntity, String> {
 
-    Optional<YieldEntity> findByUserId(String userId);
+    boolean existsByUserAssetYieldAt(String userAssetYieldAt);
 
+    @Query("{ 'user_id': ?0, 'yield_at': ?1 }")
+    List<YieldEntity> findByUserIdAndYieldAt(String userId, String yieldAt);
 }
