50959e3a4f0c55314d9cfb0c8a7804999f8e13fa
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-11-17 18:55:34 -0300

    refactor: add WalletRespositoryCustom as class extends,include findQuotaAmountByUserIdAndAssetKey method and remove addPurchaseToAssetByUserIdAndAssetName and addSaleToAssetByUserIdAndAssetName methods

--- src/main/java/personal/investwallet/modules/wallet/WalletRepository.java
+++ src/main/java/personal/investwallet/modules/wallet/WalletRepository.java
@@ -1,5 +1,6 @@
 package personal.investwallet.modules.wallet;
 
+import org.bson.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.Update;
@@ -9,23 +10,17 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface WalletRepository extends MongoRepository<WalletEntity, String> {
-
-    boolean existsByUserId(String userId);
+public interface WalletRepository extends MongoRepository<WalletEntity, String>, WalletRespositoryCustom {
 
     Optional<WalletEntity> findByUserId(String userId);
+
+    @Query(value = "{ 'userId': ?0, 'assets.?1': { $exists: true } }",
+            fields = "{ 'assets.?1.quota_amount': 1 }")
+    Optional<Document> findQuotaAmountByUserIdAndAssetKey(String userId, String assetKey);
     
     @Query("{ 'userId': ?0 }")
     @Update("{ '$set': { 'assets.?1': ?2 } }")
     void addNewAssetByUserId(String userId, String assetName, WalletEntity.Asset newAsset);
-
-    @Query("{ 'userId': ?0, 'assets.?1.assetName': ?1 }")
-    @Update("{ '$push': { 'assets.?1.purchasesInfo': ?2 }, '$inc': { 'assets.?1.quotaAmount': ?3 } }")
-    void addPurchaseToAssetByUserIdAndAssetName(String userId, String assetName, WalletEntity.Asset.PurchasesInfo purchasesInfo, int quotaIncrement);
-
-    @Query("{ 'userId': ?0, 'assets.?1.assetName': ?1 }")
-    @Update("{ '$push': { 'assets.?1.salesInfo': ?2 }, '$inc': { 'assets.?1.quotaAmount': ?3 } }")
-    void addSaleToAssetByUserIdAndAssetName(String userId, String assetName, WalletEntity.Asset.SalesInfo salesInfo, int quotaDecrease);
 
     @Query("{ 'userId': ?0, 'assets.?1.assetName': ?1 }")
     @Update("{ '$inc': { 'assets.?1.quotaAmount': ?2 } }")
