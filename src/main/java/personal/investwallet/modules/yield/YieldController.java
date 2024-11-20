332111cf74e48673bf7f47c248ab35b8070dc98c
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-11-20 12:21:39 -0300

    refactor: add yield-at route and change YieldInfoResponse to YieldInfoByYieldAtResponseDto in getManyByUserIdAndYieldAt controller method and include getManyByUserIdAndAssetName controller method

--- src/main/java/personal/investwallet/modules/yield/YieldController.java
+++ src/main/java/personal/investwallet/modules/yield/YieldController.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import personal.investwallet.modules.yield.dto.YieldInfoResponse;
+
+import personal.investwallet.modules.yield.dto.YieldAssetNameRequestDto;
+import personal.investwallet.modules.yield.dto.YieldInfoByAssetNameResponseDto;
+import personal.investwallet.modules.yield.dto.YieldInfoByYieldAtResponseDto;
 import personal.investwallet.modules.yield.dto.YieldRequestDto;
 import personal.investwallet.modules.yield.dto.YieldTimeIntervalRequestDto;
 
@@ -22,8 +25,7 @@ public class YieldController {
     @PostMapping()
     public ResponseEntity<String> createMany(
             @CookieValue(value = "access_token") String token,
-            @Valid @RequestBody List<YieldRequestDto> yields
-    ) {
+            @Valid @RequestBody List<YieldRequestDto> yields) {
 
         int result = yieldService.registerManyYieldsReceived(token, yields);
         return ResponseEntity.created(null).body("Foram registrados " + result + " dividendos com sucesso.");
@@ -32,20 +34,29 @@ public class YieldController {
     @PostMapping("/file")
     public ResponseEntity<String> createManyByCsv(
             @CookieValue(value = "access_token") String token,
-            MultipartFile file
-    ) {
+            MultipartFile file) {
 
         int result = yieldService.registerManyYieldsReceivedByCsv(token, file);
         return ResponseEntity.created(null).body("O arquivo com " + result + " linhas foi registrado com sucesso.");
     }
 
-    @GetMapping()
-    public ResponseEntity<Map<String, List<YieldInfoResponse>>> getManyByUserIdAndYieldAt(
+    @GetMapping("/yield-at")
+    public ResponseEntity<Map<String, List<YieldInfoByYieldAtResponseDto>>> getManyByUserIdAndYieldAt(
             @CookieValue(value = "access_token") String token,
-            @RequestBody YieldTimeIntervalRequestDto payload
-    ) {
+            @Valid @RequestBody YieldTimeIntervalRequestDto payload) {
 
-        Map<String, List<YieldInfoResponse>> response = yieldService.fetchAllYieldsByTimeInterval(token, payload);
+        Map<String, List<YieldInfoByYieldAtResponseDto>> response = yieldService.fetchAllYieldsByTimeInterval(token,
+                payload);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/asset-name")
+    public ResponseEntity<Map<String, List<YieldInfoByAssetNameResponseDto>>> getManyByUserIdAndAssetName(
+            @CookieValue(value = "access_token") String token,
+            @Valid @RequestBody YieldAssetNameRequestDto payload) {
+
+        Map<String, List<YieldInfoByAssetNameResponseDto>> response = yieldService.fetchAllYieldAtByAssetName(token,
+                payload);
         return ResponseEntity.ok(response);
     }
 }
