f1919e34390a8eb3695328e795b04992136350f7
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-12-15 10:30:24 -0300

    feat: include addSecurityItem and components in OpenApi

--- src/main/java/personal/investwallet/config/docs/SwaggerConfig.java
+++ src/main/java/personal/investwallet/config/docs/SwaggerConfig.java
@@ -3,9 +3,12 @@ package personal.investwallet.config.docs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,6 +20,10 @@ public class SwaggerConfig {
                 .info(new Info().title("Wallet Invest")
                         .description("API")
                         .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .addSecurityItem(new SecurityRequirement().addList("JavaInUseSecurityScheme"))
+                .components(new Components().addSecuritySchemes("JavaInUseSecurityScheme", new SecurityScheme()
+                        .name("JavaInUseSecurityScheme").type(SecurityScheme.Type.HTTP).scheme("bearer")
+                        .bearerFormat("JWT")));
     }
 }
