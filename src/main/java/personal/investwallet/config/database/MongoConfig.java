3d1d7f1579f4f25d13ed05b07a9d898e294b85b2
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-01-05 12:16:55 -0300

    refactor: remove mongoTemplate method

--- src/main/java/personal/investwallet/config/database/MongoConfig.java
+++ src/main/java/personal/investwallet/config/database/MongoConfig.java
@@ -3,19 +3,11 @@ package personal.investwallet.config.database;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import com.mongodb.client.MongoClient;
-
 @Configuration
 public class MongoConfig {
-
-    @Bean
-    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
-        return new MongoTemplate(mongoClient, "invest-wallet-mongo_db-1");
-    }
 
     @ConditionalOnMissingBean(LocalValidatorFactoryBean.class)
     @Bean
