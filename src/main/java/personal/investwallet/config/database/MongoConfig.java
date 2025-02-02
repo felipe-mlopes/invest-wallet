3fe80daab19112f9aa0143d770ff966207f72ff4
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-02-02 15:22:39 -0300

    feat: add transaction manager method

--- src/main/java/personal/investwallet/config/database/MongoConfig.java
+++ src/main/java/personal/investwallet/config/database/MongoConfig.java
@@ -3,18 +3,22 @@ package personal.investwallet.config.database;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import com.mongodb.client.MongoClient;
 
 @Configuration
+@EnableTransactionManagement
 public class MongoConfig {
 
     @Bean
     public MongoTemplate mongoTemplate(MongoClient mongoClient) {
-        return new MongoTemplate(mongoClient, "invest-wallet-mongo_db-1");
+        return new MongoTemplate(mongoClient, "invest_wallet");
     }
 
     @ConditionalOnMissingBean(LocalValidatorFactoryBean.class)
@@ -28,5 +32,10 @@ public class MongoConfig {
     public ValidatingMongoEventListener validatingMongoEventListener(
             LocalValidatorFactoryBean localValidatorFactoryBean) {
         return new ValidatingMongoEventListener(localValidatorFactoryBean);
+    }
+
+    @Bean
+    public MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
     }
 }
