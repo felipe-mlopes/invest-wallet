fe1448678cdb3db1b63abbf2bd4c2c5a36c093f8
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-02-02 15:39:18 -0300

    feat: include optional in findByAssetName method and add existsByAssetName and findAllAssetNames methods

--- src/main/java/personal/investwallet/modules/asset/AssetRepository.java
+++ src/main/java/personal/investwallet/modules/asset/AssetRepository.java
@@ -1,11 +1,19 @@
 package personal.investwallet.modules.asset;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AssetRepository extends MongoRepository<AssetEntity, String> {
 
-    AssetEntity findByAssetName(String assetName);
+    Optional<AssetEntity> findByAssetName(String assetName);
 
+    boolean existsByAssetName(String assetName);
+
+    @Query(value = "{}", fields = "{ 'assetName' : 1, '_id' : 0}")
+    Stream<String> findAllAssetNames();
 }
