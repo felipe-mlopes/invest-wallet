88a9c903e93fe5db2251728a57100414ffff7f62
Felipe Lopes <felipe.mlopes03@gmail.com>
2025-02-02 15:41:49 -0300

    feat: create asset info dto

---
+++ src/main/java/personal/investwallet/modules/asset/dto/AssetInfoDto.java
@@ -0,0 +1,11 @@
+package personal.investwallet.modules.asset.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record AssetInfoDto(
+        @NotBlank(message = "O nome do ativo não pode ser vazio") @Size(min = 5, max = 6, message = "O nome do ativo deve conter entre 5 e 6 caracteres") String assetName,
+
+        @NotBlank(message = "O nome do ativo não pode ser vazio") @Pattern(regexp = "acoes|fundos-imobiliarios", message = "O tipo do ativo deve ser 'acoes' ou 'fundos-imobiliarios'") String assetType) {
+}
